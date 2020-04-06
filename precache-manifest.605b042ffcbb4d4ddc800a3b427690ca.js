f4d1a9bfae9dd96e8289cdccbd811fa3a84e80ac
Allen Bautista <allenbautista22@gmail.com>
2020-04-06 18:14:45 +0800

    Updates

---
+++ precache-manifest.605b042ffcbb4d4ddc800a3b427690ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ece48dd271b141f47d923dc82288a7d",
+    "url": "/portfolio-project/index.html"
+  },
+  {
+    "revision": "e44d1ff5949af61ba12b",
+    "url": "/portfolio-project/static/css/main.3a3e608e.chunk.css"
+  },
+  {
+    "revision": "280f62fead79b408ecd9",
+    "url": "/portfolio-project/static/js/2.07c7fb45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-project/static/js/2.07c7fb45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44d1ff5949af61ba12b",
+    "url": "/portfolio-project/static/js/main.dadf16e5.chunk.js"
+  },
+  {
+    "revision": "d4e4b32de2ea51f7232b",
+    "url": "/portfolio-project/static/js/runtime-main.275ed2c7.js"
+  },
+  {
+    "revision": "d52e4f642be7435c2fee954d3d6ed0a2",
+    "url": "/portfolio-project/static/media/header-desktop-2.d52e4f64.jpg"
+  },
+  {
+    "revision": "2fc0d2ff0c12d6c86b10b45fda69ed28",
+    "url": "/portfolio-project/static/media/header-desktop-3.2fc0d2ff.jpg"
+  },
+  {
+    "revision": "7d6d91f2a63d7299d9c81f2ff9873dfa",
+    "url": "/portfolio-project/static/media/header-mobile.7d6d91f2.jpg"
+  }
+]);
