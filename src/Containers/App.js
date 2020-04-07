f4c40eb245ab47ca5a4c317f0bf071d0533f7ef0
Allen Bautista <allenbautista22@gmail.com>
2020-04-07 14:07:38 +0800

    Responsiveness fix and Cache Busting version 1

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -7,24 +7,36 @@ import
 } from "react-router-dom";
 
 import './App.css';
+import CacheBuster from '../CacheBuster.js';
 import NavigationBar from './NavigationBar.js';
 import Home from '../Components/Home.js';
 import Experience from '../Components/Experience';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <NavigationBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/experience" component={Experience} />
-          <Route path="/portfolio" component={Home} />
-          <Route path="/contacts" component={Home} />
-          <Route component={Home} />
-        </Switch>
-      </div>
-    </Router>
+    <CacheBuster>
+      {({ loading, isLatestVersion, refreshCacheAndReload }) => {
+        if (loading) return null;
+        if (!loading && !isLatestVersion) {
+          refreshCacheAndReload();
+        }
+
+        return (
+          <Router basename={process.env.PUBLIC_URL}>
+            <div className="App">
+              <NavigationBar />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/experience" component={Experience} />
+                <Route path="/portfolio" component={Home} />
+                <Route path="/contacts" component={Home} />
+                <Route component={Home} />
+              </Switch>
+            </div>
+          </Router>
+        );
+      }}
+    </CacheBuster>
   );
 }
 
